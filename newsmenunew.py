a585da295a133205cd69f0b1a0002b547b09bc46
Praneeth Reddy <praneeth001@gmail.com>
2019-09-06 23:21:18 +0530

    Edited menu as per the node app changes

--- newsmenunew.py
+++ newsmenunew.py
@@ -14,27 +14,21 @@ def main():
 
     # Commands for Editorials
     submenu_editorials = ConsoleMenu(
-        "Editorials/Articles", "Read editorials and articles")
+        "Editorials", "Read editorials from Top news sites")
     command_item_eds_hindu = CommandItem("The Hindu", "node index eds hindu")
     command_item_eds_et = CommandItem("Economic Times", "node index eds et")
     command_item_eds_ie = CommandItem("Indian Express", "node index eds ie")
     command_item_eds_hbl = CommandItem(
         "Hindu Business Line", "node index eds hbl")
-    command_item_eds_economist = CommandItem(
-        "Economist", "node index eds economist")
     command_item_eds_guardian = CommandItem(
         "Guardian", "node index eds guardian")
-    command_item_eds_projsyn = CommandItem(
-        "Project Syndicate", "node index projsyn all")
     submenu_editorials.append_item(command_item_eds_hindu)
     submenu_editorials.append_item(command_item_eds_et)
     submenu_editorials.append_item(command_item_eds_ie)
     submenu_editorials.append_item(command_item_eds_hbl)
-    submenu_editorials.append_item(command_item_eds_economist)
     submenu_editorials.append_item(command_item_eds_guardian)
-    submenu_editorials.append_item(command_item_eds_projsyn)
     menu_submenu_editorials = SubmenuItem(
-        "Editorials/Articles", submenu=submenu_editorials)
+        "Editorials", submenu=submenu_editorials)
     menu_submenu_editorials.set_menu(menu)
     menu.append_item(menu_submenu_editorials)
 
@@ -49,18 +43,25 @@ def main():
     menu_submenu_sports.set_menu(menu)
     menu.append_item(menu_submenu_sports)
 
-    # Commands for Management/Startups
-    submenu_mgmt = ConsoleMenu(
-        "Management/Startups", "Leadership in a gist...")
-    command_item_mgmt_hbr = CommandItem(
-        "Harvard Business Review", "node index hbr latest")
-    # command_item_mgmt_ys = CommandItem("YourStory-Social", "node index yourstory social")
-    submenu_mgmt.append_item(command_item_mgmt_hbr)
-    # submenu_mgmt.append_item(command_item_mgmt_ys)
-    menu_submenu_mgmt = SubmenuItem(
-        "Management/Startups", submenu=submenu_mgmt)
-    menu_submenu_sports.set_menu(menu)
-    menu.append_item(menu_submenu_mgmt)
+    # Commands for Magazines
+    submenu_magz = ConsoleMenu(
+        "Magazines", "Good Reads from top magazine websites")
+    command_item_magz_hbr = CommandItem(
+        "Harvard Business Review", "node index magz hbr")
+    command_item_magz_projsyn = CommandItem(
+        "Project Syndicate", "node index magz projsyn")
+    command_item_magz_economist = CommandItem(
+        "The Economist", "node index magz economist")
+    command_item_magz_atlantic = CommandItem(
+        "The Atlantic", "node index magz atlantic")
+    submenu_magz.append_item(command_item_magz_hbr)
+    submenu_magz.append_item(command_item_magz_projsyn)
+    submenu_magz.append_item(command_item_magz_economist)
+    submenu_magz.append_item(command_item_magz_atlantic)
+    menu_submenu_magz = SubmenuItem(
+        "Magazines", submenu=submenu_magz)
+    menu_submenu_magz.set_menu(menu)
+    menu.append_item(menu_submenu_magz)
 
     # Show the menu
     menu.start()
