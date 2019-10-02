08914067822d8f28297772b46e066a8c892ee43b
Praneeth Reddy <praneeth001@gmail.com>
2019-10-02 13:29:57 +0530

    added outlook opinion in magz

--- newsmenunew.py
+++ newsmenunew.py
@@ -37,20 +37,41 @@ def main():
     menu_submenu_editorials.set_menu(menu)
     menu.append_item(menu_submenu_editorials)
 
-    # Commands for Sports
-    submenu_sports = ConsoleMenu("Sports", "Read Sports Updates")
-    command_item_sports_f1_espn = CommandItem(
-        "ESPN F1", "node index sports espnf1")
-    command_item_sports_f1_autosport = CommandItem(
-        "Autosport F1", "node index sports autof1")
-    command_item_sports_epl = CommandItem(
-        "English Premier League", "node index sports epl")
-    submenu_sports.append_item(command_item_sports_f1_espn)
-    submenu_sports.append_item(command_item_sports_f1_autosport)
-    submenu_sports.append_item(command_item_sports_epl)
-    menu_submenu_sports = SubmenuItem("Sports", submenu=submenu_sports)
-    menu_submenu_sports.set_menu(menu)
-    menu.append_item(menu_submenu_sports)
+    # Commands for Magazines
+    submenu_magz = ConsoleMenu(
+        "Magazines", "Good Reads from top magazine websites")
+    command_item_magz_hbr = CommandItem(
+        "Harvard Business Review", "node index magz hbr")
+    command_item_magz_projsyn = CommandItem(
+        "Project Syndicate", "node index magz projsyn")
+    command_item_magz_economist = CommandItem(
+        "The Economist", "node index magz economist")
+    command_item_magz_atlantic = CommandItem(
+        "The Atlantic", "node index magz atlantic")
+    command_item_magz_newyorker = CommandItem(
+        "The NewYorker", "node index magz newyorker")
+    command_item_magz_frontline = CommandItem(
+        "Frontline by Hindu", "node index magz frontline")
+    command_item_magz_longreads = CommandItem(
+        "Long Reads", "node index magz longreads")
+    command_item_magz_outlook = CommandItem(
+        "Outlook", "node index magz outlook")
+    command_item_magz_hinduexplains = CommandItem(
+        "The Hindu Explains", "node index magz hinduexplains")
+    submenu_magz.append_item(command_item_magz_hbr)
+    submenu_magz.append_item(command_item_magz_projsyn)
+    submenu_magz.append_item(command_item_magz_economist)
+    submenu_magz.append_item(command_item_magz_outlook)
+    submenu_magz.append_item(command_item_magz_atlantic)
+    submenu_magz.append_item(command_item_magz_newyorker)
+    submenu_magz.append_item(command_item_magz_frontline)
+    submenu_magz.append_item(command_item_magz_longreads)
+    submenu_magz.append_item(command_item_magz_hinduexplains)
+
+    menu_submenu_magz = SubmenuItem(
+        "Magazines", submenu=submenu_magz)
+    menu_submenu_magz.set_menu(menu)
+    menu.append_item(menu_submenu_magz)
 
     # Commands for Tech News
     submenu_tech = ConsoleMenu("Science & Tech", "Read Tech News")
@@ -72,38 +93,20 @@ def main():
     menu_submenu_tech.set_menu(menu)
     menu.append_item(menu_submenu_tech)
 
-    # Commands for Magazines
-    submenu_magz = ConsoleMenu(
-        "Magazines", "Good Reads from top magazine websites")
-    command_item_magz_hbr = CommandItem(
-        "Harvard Business Review", "node index magz hbr")
-    command_item_magz_projsyn = CommandItem(
-        "Project Syndicate", "node index magz projsyn")
-    command_item_magz_economist = CommandItem(
-        "The Economist", "node index magz economist")
-    command_item_magz_atlantic = CommandItem(
-        "The Atlantic", "node index magz atlantic")
-    command_item_magz_newyorker = CommandItem(
-        "The NewYorker", "node index magz newyorker")
-    command_item_magz_frontline = CommandItem(
-        "Frontline by Hindu", "node index magz frontline")
-    command_item_magz_longreads = CommandItem(
-        "Long Reads", "node index magz longreads")
-    command_item_magz_hinduexplains = CommandItem(
-        "The Hindu Explains", "node index magz hinduexplains")
-    submenu_magz.append_item(command_item_magz_hbr)
-    submenu_magz.append_item(command_item_magz_projsyn)
-    submenu_magz.append_item(command_item_magz_economist)
-    submenu_magz.append_item(command_item_magz_atlantic)
-    submenu_magz.append_item(command_item_magz_newyorker)
-    submenu_magz.append_item(command_item_magz_frontline)
-    submenu_magz.append_item(command_item_magz_longreads)
-    submenu_magz.append_item(command_item_magz_hinduexplains)
-
-    menu_submenu_magz = SubmenuItem(
-        "Magazines", submenu=submenu_magz)
-    menu_submenu_magz.set_menu(menu)
-    menu.append_item(menu_submenu_magz)
+    # Commands for Sports
+    submenu_sports = ConsoleMenu("Sports", "Read Sports Updates")
+    command_item_sports_f1_espn = CommandItem(
+        "ESPN F1", "node index sports espnf1")
+    command_item_sports_f1_autosport = CommandItem(
+        "Autosport F1", "node index sports autof1")
+    command_item_sports_epl = CommandItem(
+        "English Premier League", "node index sports epl")
+    submenu_sports.append_item(command_item_sports_f1_espn)
+    submenu_sports.append_item(command_item_sports_f1_autosport)
+    submenu_sports.append_item(command_item_sports_epl)
+    menu_submenu_sports = SubmenuItem("Sports", submenu=submenu_sports)
+    menu_submenu_sports.set_menu(menu)
+    menu.append_item(menu_submenu_sports)
 
     # Show the menu
     menu.start()
