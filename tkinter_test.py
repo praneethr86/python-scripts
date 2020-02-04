d2bff7a8c0b7fc696c36612ececba2d5d32927c8
Praneeth Patakota <praneeth001@gmail.com>
2020-02-04 22:17:17 +0530

    trying out tkinter for the first time. tried button, but commented as we dont need it for now

--- tkinter_test.py
+++ tkinter_test.py
@@ -6,6 +6,15 @@ window.title("NewsCast for Prady")
 newslist = Label(window, text="Select Category", font=("Arial Bold", 20))
 newslist.grid(column=0, row=0)
 
+#button
+# btn = Button(window, text='Button')
+# btn.grid(column=1, row=0)
+# def clicked():
+#     newslist.configure(text="Button was clicked")
+# btn = Button(window, text='Button', command=clicked)
+
+
+
 window.geometry('1024x768')
 
 window.mainloop()
