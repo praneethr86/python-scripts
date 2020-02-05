28007928161b1a101a457cd4f06115546caaa3ca
Praneeth Patakota <praneeth001@gmail.com>
2020-02-05 22:06:38 +0530

    trying more tkinter, now have idea of what to use to build my app

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,10 +1,10 @@
 from tkinter import *
- 
+from tkinter import scrolledtext
+
 window = Tk()
 window.title("NewsCast for Prady")
 
-newslist = Label(window, text="Select Category", font=("Arial Bold", 20))
-newslist.grid(column=0, row=0)
+#Todo: use Frame/Paned Window/LabelFrame to arrange widgets, Text for showing content, scrollbar ( in both listbox and text pane), Message box for alerting.
 
 #button
 # btn = Button(window, text='Button')
@@ -13,8 +13,25 @@ newslist.grid(column=0, row=0)
 #     newslist.configure(text="Button was clicked")
 # btn = Button(window, text='Button', command=clicked)
 
-
-
 window.geometry('1024x768')
+
+# txt = scrolledtext.ScrolledText(window, width=50, height=100)
+# txt.grid(column=0, row=1)
+
+newslist = Label(window, text="Select Category", font=("Arial Bold", 20))
+
+listbox = Listbox(window)
+
+listbox.insert(1, "The Hindu")
+listbox.insert(2, "HBR")
+listbox.insert(3, "The Economist")
+listbox.insert(4, "TechCrunch")
+listbox.insert(5, "ESPN F1")
+listbox.insert(6, "EPL")
+
+newslist.pack()
+listbox.pack()
+
+
 
 window.mainloop()
